25b095b207351750219cef500e97d8cd9c090403
Matt Dawson <matthewtdawson@gmail.com>
2016-11-30 23:13:36 -0500

    Add missing eslint loader

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,19 @@
+const path = require('path');
+
+const appRootPath = require('app-root-path').toString();
 const webpack = require('webpack');
+
 const base = require('./webpack.base');
 const dev = Object.assign({}, base);
 
-//dev.debug = true;
+dev.debug = true;
 dev.devtool = 'eval';
 dev.entry.unshift('webpack-hot-middleware/client');
+dev.module.preLoaders = [{
+    test: /\.js$/,
+    loader: 'eslint-loader',
+    include: [path.resolve(appRootPath, 'src/js')]
+}];
 dev.plugins = [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
